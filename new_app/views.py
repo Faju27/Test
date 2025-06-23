f10e0bc03242a626776219354da76b4f015a5a4a
Fajar <fajartk27@gmail.com>
2025-06-23 10:48:26 +0530

    pagination

--- new_app/views.py
+++ new_app/views.py
@@ -1,3 +1,5 @@
+from django.core.paginator import Paginator
+from django.db.models import Q
 from django.shortcuts import render, redirect
 
 from new_app.forms import BloggerForm
@@ -6,8 +8,17 @@ from new_app.models import Blogger
 
 # Create your views here.
 def home(request):
+    query = request.GET.get('q','')
     blogger = Blogger.objects.all()
-    return render(request, 'home.html' , {"blogger": blogger})
+    if query:
+        blogger = blogger.filter(
+            Q(name__icontains=query) | Q(phone__icontains=query)
+        )
+
+    paginator = Paginator(blogger, 2)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'home.html' , {"page_obj": page_obj, 'query':query })
 
 
 def register(request):
